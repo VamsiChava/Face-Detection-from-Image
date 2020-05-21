fff2a3536ea7b98ab9b8927374b62277cc7b87ef
VamsiChava <36822542+VamsiChava@users.noreply.github.com>
2020-05-21 13:20:00 -0400

    EyeDetectionV0_0.1

---
+++ eyeDetection.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2 as cv
+import imutils
+
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+eye_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_eye.xml")
+img = cv.imread('people_with_phones.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+
+img = imutils.resize(img, width=450)
+cv.imshow("InputImage", img)		
+cv.waitKey(0)
+cv.destroyAllWindows()
